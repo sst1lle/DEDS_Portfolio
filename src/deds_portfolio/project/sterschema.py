e70a18554221cc861df36a15045d9775042e9d24
Sam Stille <samstille2712@yahoo.nl>
2025-03-14 18:34:54 +0100

    lesweek 3 af

--- src/deds_portfolio/project/sterschema.py
+++ src/deds_portfolio/project/sterschema.py
@@ -22,8 +22,8 @@ fact_tables = {
 }
 
 for fact, attributes in fact_tables.items():
-    label = f"{fact}|" + "\\l".join(attributes) + "\\l"
-    dot.node(fact, label=f"{{ {label} }}", shape="record", style="filled", fillcolor="lightyellow")
+    label = f"{{ {fact} | " + " \\l".join(attributes) + " \\l}}"
+    dot.node(fact, label=label, shape="record", style="filled", fillcolor="lightyellow")
 
 ### DIMENSIETABELLEN (Groen) ###
 dimension_tables = {
@@ -48,39 +48,39 @@ dimension_tables = {
 }
 
 for dim, attributes in dimension_tables.items():
-    label = f"{dim}|" + "\\l".join(attributes) + "\\l"
-    dot.node(dim, label=f"{{ {label} }}", shape="record", style="filled", fillcolor="lightgreen")
+    label = f"{{ {dim} | " + " \\l".join(attributes) + " \\l}}"
+    dot.node(dim, label=label, shape="record", style="filled", fillcolor="lightgreen")
 
 ### RELATIES TUSSEN FEITEN EN DIMENSIES ###
-dot.edge("Order_Details", "Order_Header", label="Order_ID")
-dot.edge("Order_Details", "Product", label="Product_ID")
+dot.edge("Order_Details", "Order_Header")
+dot.edge("Order_Details", "Product")
 
-dot.edge("Returned_Item", "Order_Details", label="Order_Details_ID")
-dot.edge("Returned_Item", "Return_Reason", label="Return_Reason_ID")
+dot.edge("Returned_Item", "Order_Details")
+dot.edge("Returned_Item", "Return_Reason")
 
-dot.edge("Sales_Demographic", "Retailer_Headquarters", label="Retailer_HQ_ID")
-dot.edge("Sales_Demographic", "Age_Group", label="Age_Group_ID")
+dot.edge("Sales_Demographic", "Retailer_Headquarters")
+dot.edge("Sales_Demographic", "Age_Group")
 
-dot.edge("Inventory_Levels", "Product", label="Product_ID")
-dot.edge("Inventory_Levels", "Tijd", label="Inventory_Year, Inventory_Month")
+dot.edge("Inventory_Levels", "Product")
+dot.edge("Inventory_Levels", "Tijd")
 
-dot.edge("Product_Forecast", "Product", label="Product_ID")
-dot.edge("Product_Forecast", "Tijd", label="Forecast_Year, Forecast_Month")
+dot.edge("Product_Forecast", "Product")
+dot.edge("Product_Forecast", "Tijd")
 
-dot.edge("Satisfaction", "Sales_Staff", label="Sales_Staff_ID")
-dot.edge("Satisfaction", "Satisfaction_Type", label="Satisfaction_Type_ID")
-dot.edge("Satisfaction", "Tijd", label="Satisfaction_Year")
+dot.edge("Satisfaction", "Sales_Staff")
+dot.edge("Satisfaction", "Satisfaction_Type")
+dot.edge("Satisfaction", "Tijd")
 
 ### RELATIES TUSSEN DIMENSIETABELLEN ###
-dot.edge("Retailer_Headquarters", "Retailer", label="Retailer_ID")
-dot.edge("Retailer", "Retailer_Type", label="Retailer_Type_ID")
-dot.edge("Retailer_Type", "Retailer_Segment", label="Retailer_Segment_ID")
-dot.edge("Country", "Sales_Territory", label="Country_ID")
-dot.edge("Product", "Product_Type", label="Product_Type_ID")
-dot.edge("Product_Type", "Product_Line", label="Product_Line_ID")
-dot.edge("Sales_Staff", "Sales_Branch", label="Sales_Branch_ID")
-dot.edge("Sales_Staff", "Course", label="Course_ID")
-dot.edge("Order_Header", "Order_Method", label="Order_Method_ID")
+dot.edge("Retailer_Headquarters", "Retailer")
+dot.edge("Retailer", "Retailer_Type")
+dot.edge("Retailer_Type", "Retailer_Segment")
+dot.edge("Country", "Sales_Territory")
+dot.edge("Product", "Product_Type")
+dot.edge("Product_Type", "Product_Line")
+dot.edge("Sales_Staff", "Sales_Branch")
+dot.edge("Sales_Staff", "Course")
+dot.edge("Order_Header", "Order_Method")
 
 # Diagram opslaan als PNG
 dot.render(diagram_path)
